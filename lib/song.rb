3958615a48d41304207d91346f2f0b916f579866
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-07 19:51:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,10 +21,12 @@ class Song
     song.name = name
     song
   end
+  
   def self.create_by_name(name)
     song = self.new
     song.name = name
     @@all << song
+    song
   end
 
   # def self.find_by_name(name)
