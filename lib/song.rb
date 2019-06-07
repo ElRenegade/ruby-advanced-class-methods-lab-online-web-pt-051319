8717f19520c7794b7372ac4f189859e0322233b0
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-07 20:08:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,13 @@ class Song
   def self.find_by_name(name)
     @@all.find{|x| x.name == name }
   end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name
+    end
+    
+  end
 end
